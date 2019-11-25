0b6189b1f73cba84fa9b498afef9ada9ec0bfe4a
Adrian <Chlebosz>
2019-11-25 01:06:09 +0100

    initial backend commit

---
+++ Elka-Drive-Backend/src/test/java/pl/edu/pw/elka/zpr/elkadrive/ElkadriveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.elka.zpr.elkadrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkadriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
